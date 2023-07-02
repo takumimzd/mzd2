10d80c5a73dd3a4a10678a9cd246d89d95bc5422
takumimzd <retnuh.takumi@gmail.com>
2023-07-02 21:19:47 +0900

    fix: Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,16 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import React from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <React.StrictMode>
+      <Head>
+        <title>Me</title>
+        <meta name='description' content='introduction' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <Component {...pageProps} />
+    </React.StrictMode>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
-import Head from 'next/head';
-
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Me</title>
-        <meta name='description' content='introduction' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-      </Head>
+      <h1>Home</h1>
     </>
   );
 }
