ce58e6f42f5d8d8870ee4e744ff53cd24ffebe04
takumimzd <retnuh.takumi@gmail.com>
2023-07-02 18:46:09 +0900

    initial-setting

---
+++ .lintstagedrc.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const buildEslintCommand = (filenames) =>
+  `yarn lint:fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
+
+const buildPrettierCommand = (filenames) =>
+  `yarn format --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildPrettierCommand],
+};
